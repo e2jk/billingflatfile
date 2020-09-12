53ad23be7fd2b75213876a952cf7f58dced0b5e3
Emilien Klein <emilien@klein.st>
2020-09-12 11:30:10 +0200

    Include flake8 to test suite

--- test.bat
+++ test.bat
@@ -1,4 +1,6 @@
 @echo off
 coverage run --include=./*.py --omit=tests/*,.venv-billingflatfile/* -m unittest discover || EXIT /B 1
+flake8 billingflatfile.py --statistics --count || EXIT /B 1
+flake8 tests --statistics --count || EXIT /B 1
 rd /s /q html_dev\coverage
 coverage html --directory=html_dev\coverage --title="Code test coverage for billingflatfile"
--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 coverage run --include=./*.py --omit=tests/*,.venv-billingflatfile/* -m unittest discover && \
+flake8 billingflatfile.py --statistics --count && \
+flake8 tests --statistics --count && \
 rm -rf html_dev/coverage && \
 coverage html --directory=html_dev/coverage --title="Code test coverage for billingflatfile"
