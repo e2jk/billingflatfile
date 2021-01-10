82114c0c9005a931441903b8da7c6fc8e28ea4ba
Emilien Klein <emilien@klein.st>
2021-01-10 21:50:47 +0100

    Update pipenv command for CI

--- test.bat
+++ test.bat
@@ -4,3 +4,4 @@ flake8 billingflatfile.py --statistics --count || EXIT /B 1
 flake8 tests --statistics --count || EXIT /B 1
 rd /s /q html_dev\coverage
 coverage html --directory=html_dev\coverage --title="Code test coverage for billingflatfile"
+coverage xml
